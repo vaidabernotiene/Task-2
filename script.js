2b264288dbef1d868a1f3d79ac46b89a8acd9832
vaidabernotiene <vaidabernotiene@yahoo.com>
2023-02-19 16:25:44 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+/* ------------------------------ TASK 2 ----------------------------
+Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
+su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
+pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
+------------------------------------------------------------------- */
+const btnEl = document.querySelector("#btn__element");
+const clickCountEl = document.querySelector("#btn__state");
